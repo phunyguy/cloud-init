49a51a0af75f321da48c5e86ef09490cf28097b4
Brett Holman <bholman.devel@gmail.com>
2021-11-08 14:55:38 -0700

    wip

--- tests/integration_tests/modules/test_grow_part.py
+++ tests/integration_tests/modules/test_grow_part.py
@@ -61,7 +61,7 @@ mounts:
 @pytest.mark.lxd_setup.with_args(setup_and_mount_lxd_disk)
 @pytest.mark.ubuntu
 @pytest.mark.lxd_vm
-class TestDeviceAliases:
+class TestGrowPart:
     """Test devices aliases work on disk setup/mount"""
 
     def test_device_alias(self, create_disk, client: IntegrationInstance):
@@ -82,120 +82,120 @@ class TestDeviceAliases:
         assert sdb['children'][1]['mountpoint'] == '/mnt2'
 
 
-PARTPROBE_USERDATA = """\
-#cloud-config
-disk_setup:
-  /dev/sdb:
-    table_type: mbr
-    layout: [50, 50]
-    overwrite: True
-fs_setup:
-  - label: test
-    device: /dev/sdb1
-    filesystem: ext4
-  - label: test2
-    device: /dev/sdb2
-    filesystem: ext4
-mounts:
-- ["/dev/sdb1", "/mnt1"]
-- ["/dev/sdb2", "/mnt2"]
-"""
-
-UPDATED_PARTPROBE_USERDATA = """\
-#cloud-config
-disk_setup:
-  /dev/sdb:
-    table_type: mbr
-    layout: [100]
-    overwrite: True
-fs_setup:
-  - label: test3
-    device: /dev/sdb1
-    filesystem: ext4
-mounts:
-- ["/dev/sdb1", "/mnt3"]
-"""
-
-
-@pytest.mark.user_data(PARTPROBE_USERDATA)
-@pytest.mark.lxd_setup.with_args(setup_and_mount_lxd_disk)
-@pytest.mark.ubuntu
-@pytest.mark.lxd_vm
-class TestPartProbeAvailability:
-    """Test disk setup works with partprobe
-
-    Disk setup can run successfully on a mounted partition when
-    partprobe is being used.
-
-    lp-1920939
-    """
-
-    def _verify_first_disk_setup(self, client, log):
-        verify_clean_log(log)
-        lsblk = json.loads(client.execute('lsblk --json'))
-        sdb = [x for x in lsblk['blockdevices'] if x['name'] == 'sdb'][0]
-        assert len(sdb['children']) == 2
-        assert sdb['children'][0]['name'] == 'sdb1'
-        assert sdb['children'][0]['mountpoint'] == '/mnt1'
-        assert sdb['children'][1]['name'] == 'sdb2'
-        assert sdb['children'][1]['mountpoint'] == '/mnt2'
-
-    # Not bionic or xenial because the LXD agent gets in the way of us
-    # changing the userdata
-    @pytest.mark.not_bionic
-    @pytest.mark.not_xenial
-    def test_disk_setup_when_mounted(
-        self, create_disk, client: IntegrationInstance
-    ):
-        """Test lp-1920939.
-
-        We insert an extra disk into our VM, format it to have two partitions,
-        modify our cloud config to mount devices before disk setup, and modify
-        our userdata to setup a single partition on the disk.
-
-        This allows cloud-init to attempt disk setup on a mounted partition.
-        When blockdev is in use, it will fail with
-        "blockdev: ioctl error on BLKRRPART: Device or resource busy" along
-        with a warning and a traceback. When partprobe is in use, everything
-        should work successfully.
-        """
-        log = client.read_from_file('/var/log/cloud-init.log')
-        self._verify_first_disk_setup(client, log)
-
-        # Update our userdata and cloud.cfg to mount then perform new disk
-        # setup
-        client.write_to_file(
-            '/var/lib/cloud/seed/nocloud-net/user-data',
-            UPDATED_PARTPROBE_USERDATA,
-        )
-        client.execute(
-            "sed -i 's/write-files/write-files\\n - mounts/' "
-            "/etc/cloud/cloud.cfg"
-        )
-
-        client.execute('cloud-init clean --logs')
-        client.restart()
-
-        # Assert new setup works as expected
-        verify_clean_log(log)
-
-        lsblk = json.loads(client.execute('lsblk --json'))
-        sdb = [x for x in lsblk['blockdevices'] if x['name'] == 'sdb'][0]
-        assert len(sdb['children']) == 1
-        assert sdb['children'][0]['name'] == 'sdb1'
-        assert sdb['children'][0]['mountpoint'] == '/mnt3'
-
-    def test_disk_setup_no_partprobe(
-        self, create_disk, client: IntegrationInstance
-    ):
-        """Ensure disk setup still works as expected without partprobe."""
-        # We can't do this part in a bootcmd because the path has already
-        # been found by the time we get to the bootcmd
-        client.execute('rm $(which partprobe)')
-        client.execute('cloud-init clean --logs')
-        client.restart()
-
-        log = client.read_from_file('/var/log/cloud-init.log')
-        self._verify_first_disk_setup(client, log)
-
-        assert 'partprobe' not in log
+#PARTPROBE_USERDATA = """\
+##cloud-config
+#disk_setup:
+#  /dev/sdb:
+#    table_type: mbr
+#    layout: [50, 50]
+#    overwrite: True
+#fs_setup:
+#  - label: test
+#    device: /dev/sdb1
+#    filesystem: ext4
+#  - label: test2
+#    device: /dev/sdb2
+#    filesystem: ext4
+#mounts:
+#- ["/dev/sdb1", "/mnt1"]
+#- ["/dev/sdb2", "/mnt2"]
+#"""
+#
+#UPDATED_PARTPROBE_USERDATA = """\
+##cloud-config
+#disk_setup:
+#  /dev/sdb:
+#    table_type: mbr
+#    layout: [100]
+#    overwrite: True
+#fs_setup:
+#  - label: test3
+#    device: /dev/sdb1
+#    filesystem: ext4
+#mounts:
+#- ["/dev/sdb1", "/mnt3"]
+#"""
+#
+#
+#@pytest.mark.user_data(PARTPROBE_USERDATA)
+#@pytest.mark.lxd_setup.with_args(setup_and_mount_lxd_disk)
+#@pytest.mark.ubuntu
+#@pytest.mark.lxd_vm
+#class TestPartProbeAvailability:
+#    """Test disk setup works with partprobe
+#
+#    Disk setup can run successfully on a mounted partition when
+#    partprobe is being used.
+#
+#    lp-1920939
+#    """
+#
+#    def _verify_first_disk_setup(self, client, log):
+#        verify_clean_log(log)
+#        lsblk = json.loads(client.execute('lsblk --json'))
+#        sdb = [x for x in lsblk['blockdevices'] if x['name'] == 'sdb'][0]
+#        assert len(sdb['children']) == 2
+#        assert sdb['children'][0]['name'] == 'sdb1'
+#        assert sdb['children'][0]['mountpoint'] == '/mnt1'
+#        assert sdb['children'][1]['name'] == 'sdb2'
+#        assert sdb['children'][1]['mountpoint'] == '/mnt2'
+#
+#    # Not bionic or xenial because the LXD agent gets in the way of us
+#    # changing the userdata
+#    @pytest.mark.not_bionic
+#    @pytest.mark.not_xenial
+#    def test_disk_setup_when_mounted(
+#        self, create_disk, client: IntegrationInstance
+#    ):
+#        """Test lp-1920939.
+#
+#        We insert an extra disk into our VM, format it to have two partitions,
+#        modify our cloud config to mount devices before disk setup, and modify
+#        our userdata to setup a single partition on the disk.
+#
+#        This allows cloud-init to attempt disk setup on a mounted partition.
+#        When blockdev is in use, it will fail with
+#        "blockdev: ioctl error on BLKRRPART: Device or resource busy" along
+#        with a warning and a traceback. When partprobe is in use, everything
+#        should work successfully.
+#        """
+#        log = client.read_from_file('/var/log/cloud-init.log')
+#        self._verify_first_disk_setup(client, log)
+#
+#        # Update our userdata and cloud.cfg to mount then perform new disk
+#        # setup
+#        client.write_to_file(
+#            '/var/lib/cloud/seed/nocloud-net/user-data',
+#            UPDATED_PARTPROBE_USERDATA,
+#        )
+#        client.execute(
+#            "sed -i 's/write-files/write-files\\n - mounts/' "
+#            "/etc/cloud/cloud.cfg"
+#        )
+#
+#        client.execute('cloud-init clean --logs')
+#        client.restart()
+#
+#        # Assert new setup works as expected
+#        verify_clean_log(log)
+#
+#        lsblk = json.loads(client.execute('lsblk --json'))
+#        sdb = [x for x in lsblk['blockdevices'] if x['name'] == 'sdb'][0]
+#        assert len(sdb['children']) == 1
+#        assert sdb['children'][0]['name'] == 'sdb1'
+#        assert sdb['children'][0]['mountpoint'] == '/mnt3'
+#
+#    def test_disk_setup_no_partprobe(
+#        self, create_disk, client: IntegrationInstance
+#    ):
+#        """Ensure disk setup still works as expected without partprobe."""
+#        # We can't do this part in a bootcmd because the path has already
+#        # been found by the time we get to the bootcmd
+#        client.execute('rm $(which partprobe)')
+#        client.execute('cloud-init clean --logs')
+#        client.restart()
+#
+#        log = client.read_from_file('/var/log/cloud-init.log')
+#        self._verify_first_disk_setup(client, log)
+#
+#        assert 'partprobe' not in log
